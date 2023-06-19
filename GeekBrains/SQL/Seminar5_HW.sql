0134a0da2b073648413e6cd2b6fe1f4c0651177d
Evgeniy Kopylov <evgenijkopylov19540@gmail.com>
2023-06-19 11:45:46 +0300

    Fixed 5HW in SQL

--- GeekBrains/SQL/Seminar5_HW.sql
+++ GeekBrains/SQL/Seminar5_HW.sql
@@ -143,7 +143,7 @@ VALUES
 	('2020-02-12 07:35:38', 1);
 
 -- Вывести название и цену для всех анализов, которые продавались 5 февраля 2020 и всю следующую неделю.
-SELECT analysis.an_name, orders.ord_datetime
+SELECT analysis.an_name, orders.ord_datetime, analysis.an_price
 FROM analysis
 LEFT JOIN orders
 ON analysis.an_id = orders.ord_an
