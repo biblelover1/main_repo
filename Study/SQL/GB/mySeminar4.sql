eee076332e0cf4be386e81e9ad51b76444023589
Evgeniy K <evgenijkopylov19540@gmail.com>
2023-11-24 21:02:52 +0300

    Organized folders
    HW1 added in business process

---
+++ Study/SQL/GB/mySeminar4.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users
+(
+    id  int auto_increment primary key,
+    login varchar(255) null,
+    pass  varchar(255) null,
+    male  tinyint      null
+);
+
+DROP TABLE IF EXISTS clients;
+CREATE TABLE IF NOT EXISTS clients
+(
+    id    int auto_increment primary key,
+    login varchar(255) null,
+    pass  varchar(255) null,
+    male  tinyint      null
+);
+
+INSERT INTO users (login, pass, male) 
+VALUES 
+('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1),
+('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1),
+ ('Olia', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2),
+ ('Tom', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1),
+('Margaret', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2);
+
+INSERT INTO clients (login, pass, male) VALUES ('alexander', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1),
+ ('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1),
+('Olia', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2),
+('Dmitry', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1),
+ ('Margaret', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2),
+('Leonid', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1),
+ ('Olga', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2),
+ ('Tom', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1),
+('Masha', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2),
+ ('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+ 
+ 
+--
+SELECT login, pass FROM clients
+UNION
+SELECT login, pass FROM users;
+--
+SELECT * FROM clients
+UNION ALL
+SELECT * FROM users;
+
+--
+SELECT clients.login, users.login, clients.pass, users.pass  FROM clients
+INNER JOIN users
+ON clients.login = users.login
+
